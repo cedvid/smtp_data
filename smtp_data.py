e6bf6bcf91af4fcb982a19e9fee5410087a65124
cedvid <cedricvidot@gmail.com>
2023-02-08 14:40:50 +0100

    Group smtp by color

--- smtp_data.py
+++ smtp_data.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def get_color(smtp_code):
+    if smtp_code == 'other':
+        return 'gray'
+    elif 200 <= smtp_code < 400:
+        return 'green'
+    elif 400 <= smtp_code < 500:
+        return 'orange'
+    elif 500 <= smtp_code < 600:
+        return 'red'
+    else:
+        return 'black'
+
 data = pd.read_csv('smtpdata.csv')
 data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
 data['month'] = data['timestamp'].dt.strftime('%b')
 month_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 data['month'] = pd.Categorical(data['month'], categories=month_order, ordered=True)
 grouped_data = data.groupby('month').mean()
-grouped_data.plot(kind='bar')
+
+smtp_codes = [col.split(',')[0] for col in grouped_data.columns]
+smtp_codes = [int(code) if code.isdigit() else 'other' for code in smtp_codes]
+
+grouped_data.plot(kind='bar', color=[get_color(code) for code in smtp_codes])
 plt.show()
