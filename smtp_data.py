c3f9c3cf279053a2b284e0d685a347e847b08c62
cedvid <cedricvidot@gmail.com>
2023-02-08 15:42:52 +0100

    Remove months with no data

--- smtp_data.py
+++ smtp_data.py
@@ -8,20 +8,23 @@ def get_color(smtp_code):
         return 'green'
     elif 400 <= smtp_code < 500:
         return 'orange'
-    elif 500 <= smtp_code < 600:
+    elif 500 <= smtp_code < 699:
         return 'red'
     else:
-        return 'black'
+        return 'grey'
 
 data = pd.read_csv('smtpdata.csv')
 data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
 data['month'] = data['timestamp'].dt.strftime('%b')
-month_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+month_order = ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
 data['month'] = pd.Categorical(data['month'], categories=month_order, ordered=True)
+
 grouped_data = data.groupby('month').mean()
 
 smtp_codes = [col.split(',')[0] for col in grouped_data.columns]
 smtp_codes = [int(code) if code.isdigit() else 'other' for code in smtp_codes]
 
 grouped_data.plot(kind='bar', color=[get_color(code) for code in smtp_codes])
+
 plt.show()
